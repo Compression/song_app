e73d839dfdf367ee5be97e10eff7344cb0ba1f91
Josh <rkb.josh@gmail.com>
2014-10-09 20:53:56 -0400

    added ability to add and delete songs from secure page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,5 @@
 class SongsController < ApplicationController
-
+  http_basic_authenticate_with name:"admin", password: ENV["EDIT_PASSWORD"]
   def index
     @song = Song.new
     @songs = Song.all
@@ -17,9 +17,24 @@ class SongsController < ApplicationController
     end
   end
 
+  def edit
+    @songs = Song.all
+  end
+
+  def destroy
+    song = Song.find(params[:id])
+    if song.destroy
+      flash[:notice] = "You've successfully deleted this bullshit."
+    else
+      flash[:alert] = "oh WTF. didn't work."
+    end
+
+    redirect_to "/songs/1/edit"
+  end
+
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist, :bpm, :genre)
+    params.require(:song, :id).permit(:title, :artist, :bpm, :genre)
   end
 end
